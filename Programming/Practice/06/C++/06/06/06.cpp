766e0fb89ccd9b3600f277b191d38f8b19fdcd1e
Jujuba2002 <71664025+Jujuba2002@users.noreply.github.com>
2020-12-11 18:21:10 +0300

    Add files via upload

---
+++ Programming/Practice/06/C++/06/06/06.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    double(a);
+    double(b);
+    double(c);
+    double(D);
+    double(x1);
+    double(x2);
+    cout << "Введите a:" << endl;
+    cin >> a;
+    cout << "Введите b:" << endl;
+    cin >> b;
+    cout << "Введите c:" << endl;
+    cin >> c;
+    D = sqrt(b * b - 4 * a * c);
+    if ((b * b - 4 * a * c) < 0)
+    {
+        cout << "Решений нет";
+    }
+    else {
+        x1 = ((-b + D) / 2);
+        x2 = ((-b - D) / 2);
+        if (x1 == x2)
+        {
+            cout << "x = ";
+            cout << x1;
+        }
+        else
+        {
+            cout << "x1 = ";
+            cout << x1 << endl;
+            cout << "x2 = ";
+            cout << x2 << endl;
+        }
+    }
+}
