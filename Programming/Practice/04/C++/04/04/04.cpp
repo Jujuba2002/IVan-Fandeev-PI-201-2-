79f9a0f74a0ff1170929d1815d30997ab8fe0b5a
Jujuba2002 <71664025+Jujuba2002@users.noreply.github.com>
2020-12-11 18:18:18 +0300

    Add files via upload

---
+++ Programming/Practice/04/C++/04/04/04.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUSSIAN");
+	cout << "Вариант программы с помощью дополнительной переменной:" << endl;
+	double(a);
+	double(b);
+	double(c);
+	cout << "Введите числа a и b:" << endl;
+	cin >> a;
+	cin >> b;
+	cout << "a = ";
+	cout << double(a) << endl;
+	cout << "b = ";
+	cout << double(b) << endl;
+	c = a;
+	a = b;
+	b = c;
+	cout << "После выполнения программы a и b поменялись значениями:" << endl;
+	cout << "a = ",
+		cout << double(a) << endl;
+	cout << "b = ",
+		cout << double(b) << endl;
+	cout << "";
+	cout << "";
+	cout << "";
+	cout << "Вариант программы без помощи дополнительной переменной:" << endl;
+	cout << "Введите числа a и b:" << endl;
+	cin >> a;
+	cin >> b;
+	cout << "a = ";
+	cout << double(a) << endl;
+	cout << "b = ";
+	cout << double(b) << endl;
+	swap(a, b);
+	cout << "После выполнения программы a и b поменялись значениями:" << endl;
+	cout << "a = ";
+	cout << double(a) << endl;
+	cout << "b = ";
+	cout << double(b) << endl;
+}
