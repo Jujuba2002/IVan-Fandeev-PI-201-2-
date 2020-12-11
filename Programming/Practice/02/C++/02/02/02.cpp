fceec4928d48c54fa9ec17eb834087318aa67464
Jujuba2002 <71664025+Jujuba2002@users.noreply.github.com>
2020-12-11 18:14:25 +0300

    Add files via upload

---
+++ Programming/Practice/02/C++/02/02/02.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    int a;
+    int b;
+    double c;
+    double d;
+    a = 3;
+    b = 3.14;
+    c = 3;
+    d = 3.14;
+    cout << "a = ";
+    cout << int(a) << endl;
+    cout << "b = ";
+    cout << int(b) << endl;
+    cout << "b = 3, потому что она типа int, дробная часть пропала" << endl;
+    cout << "c = ";
+    cout << double(c) << endl;
+    cout << "d = ";
+    cout << double(d) << endl;
+}
